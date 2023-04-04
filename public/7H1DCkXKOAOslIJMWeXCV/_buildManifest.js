e21b01eada2d662a6e2ee199c236a99d5b324807
Corgi Shortleg <corgishortleg114@gmail.com>
2023-04-04 12:16:24 +0900

    2nd commit

---
+++ public/7H1DCkXKOAOslIJMWeXCV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/127-17fecd596588c12d.js","static/chunks/pages/index-4b4650c356b7a336.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
